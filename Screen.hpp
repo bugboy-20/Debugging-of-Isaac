195dd4f9781e41d4d06faf7ad7c253403ac46cac
jantizio <emanuele0246@gmail.com>
2022-03-20 11:19:21 +0100

    proposta schermo

--- Screen.hpp
+++ Screen.hpp
@@ -1,9 +1,16 @@
+#include <ncursesw/ncurses.h>
+
 class Screen
 {
 private:
-    WINDOW main;
+    WINDOW room;
 
 public:
-    Screen(room r);
-    renderMain(room r);
+    Screen();
+    // stampa sullo schermo l'intera stanza
+    renderRoom(room r);
+
+    // stampa sullo schermp solo gli elementi che possono spostarsi (muri ecc)
+    updateRoom(room r); // per ora propongo entrambi i metodi
+                        // ma si può anche scegliere di usare un solo metodo se preferite
 };
