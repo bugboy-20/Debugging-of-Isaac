43053b3718bd2f38b1dac141bf1e4d8b5532755b
asdru22 <76167257+asdru22@users.noreply.github.com>
2022-03-26 11:40:06 +0100

    Update Entity.hpp

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -13,10 +13,17 @@ public:
         this->health=0;
         this->damage=0;
     };
-
-    bool move_up(room room);    //chiede una copia della "planimetria" della stanza
-                                //cerca di muovere l'entità, restituisce true qualora la cosa abbia sucesso
-                    // btw se hai non hai idee su come implementare questo metodo, una proposta l'avevo scritta sul main
+//chiede una copia della "planimetria" della stanza
+//cerca di muovere l'entità, restituisce true qualora la cosa abbia sucesso
+// btw se hai non hai idee su come implementare questo metodo, una proposta l'avevo scritta sul main
+// collision è un metodo fatto da Saad che ritorna True quando nella posizione c'è già un oggetto incompenetrabile della stanza    
+    bool move_up(room room){   
+        move( const room r, int x, int y) {
+            if(!collision(this->x,this->y,r)){
+                this->y += 1;
+                return true;
+                    } else return false;
+                }
     bool move_down(room room);
     bool move_left(room room);
     bool move_right(room room);
