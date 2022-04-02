367c9313485abb1267626e23644609277f958332
asdru22 <76167257+asdru22@users.noreply.github.com>
2022-04-02 19:00:42 +0200

    cambiamenti a entity

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -10,26 +10,10 @@ class Entity: public Core {
     int health;
     int damage;
 
-    public: Entity(int health, int damage) { // costruttore spero
-      this -> health = 0;
-      this -> damage = 0;
-      bool move_up(room room) {
-        return move(room, 0, -1);
-      };
-      bool move_down(room room) {
-        return move(room, 0, 1);
-      };
-      bool move_left(room room) {
-        return move(room, 1, 0);
-      };
-      bool move_right(room room) {
-        return move(room, -1, 0);
-      };
-    };
-    private: bool move(const room r, int x, int y) {
-      if (!collision(this -> pos.x + x, this -> pos.y + y, r)) {
-        this -> pos.x += x;
-        this -> pos.y += y;
-        return true;
-      } else return false;
-    }
+    public: Entity(int health, int damage);
+      bool move_up(room room);
+      bool move_down(room room);
+      bool move_left(room room);
+      bool move_right(room room);
+      private: bool move(const room r, int x, int y);
+};
--- src/Entity.cpp
+++ src/Entity.cpp
@@ -3,16 +3,23 @@
 Entity::Entity(){
   health = 0;
   damage = 0;
-  bool move_up(room room) {
+};
+  bool Entity::move_up(room room) {
     return move(room, 0, -1);
   };
-  bool move_down(room room) {
+  bool Entity::move_down(room room) {
     return move(room, 0, 1);
   };
-  bool move_left(room room) {
+  bool Entity::move_left(room room) {
     return move(room, 1, 0);
   };
-  bool move_right(room room) {
+  bool Entity::move_right(room room) {
     return move(room, -1, 0);
   };
+bool Entity::move(const room r, int x, int y){
+   if(!collision(this->pos.x+x,this->pos.y+y,r)) {
+     this->pos.x += x;
+     this->pos.y += y;
+     return true;
+  } else return false;
 };
