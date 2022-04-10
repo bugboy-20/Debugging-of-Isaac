e4edb7a822b6ea7ebe0f61081d82f7b10ba9938b
SaadMedhat <saad2001f@gmail.com>
2022-04-10 11:21:53 +0200

    Funzione per gestire la collisione con le mura

--- src/physics.cpp
+++ src/physics.cpp
@@ -1,7 +1,18 @@
 #include "physics.h"
+
 bool collision(int x, int y, room r)
 {
-    return false;
+
+    if(y < 1){
+        return true;
+    }else if(x < 1){
+        return true;
+    }else if(y > 18){
+        return true;
+    }else if( x > 98){
+        return true;
+    }else return false;
+
 }
 
 void do_room(room *r){}; // fa cose sulla stanza
