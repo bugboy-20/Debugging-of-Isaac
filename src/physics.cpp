b9d082991c99558a56f8d04b63b61dc7e7750021
SaadMedhat <saad2001f@gmail.com>
2022-04-11 07:58:24 +0200

    "Generalizzazione" della funzione collision

--- src/physics.cpp
+++ src/physics.cpp
@@ -3,15 +3,11 @@
 bool collision(int x, int y, room r)
 {
 
-    if(y < 1){
-        return true;
-    }else if(x < 1){
-        return true;
-    }else if(y > 18){
-        return true;
-    }else if( x > 98){
-        return true;
-    }else return false;
+    if(y < 1) return true;
+    else if(x < 1) return true;
+    else if(y > ROOM_HEIGHT - 2) return true;
+    else if( x > ROOM_WIDTH - 2) return true;
+    else return false;
 
 }
 
