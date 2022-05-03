8a8b51c945351bbb8ff7776ada49b4b347fef2f7
SaadMedhat <saad2001f@gmail.com>
2022-05-03 20:22:26 +0200

    Rvisitazione di collision Collision chiama altre funzioni

--- include/physics.h
+++ include/physics.h
@@ -4,6 +4,8 @@
 
 bool collision(int x, int y, Room r);
 
+bool door_collision(int x, int y, Room r);
+
 void do_room(Room *r); // fa cose sulla stanza
 
 bool game_over();
--- src/physics.cpp
+++ src/physics.cpp
@@ -3,12 +3,16 @@
 bool collision(int x, int y, Room r)
 {
 
+    door_collision(x, y, r);
+
+}
+
+bool door_collision(int x, int y, Room r){
     if(y < 1) return true;
     else if(x < 1) return true;
     else if(y > ROOM_HEIGHT - 2) return true;
     else if( x > ROOM_WIDTH - 2) return true;
     else return false;
-
 }
 
 void do_room(Room *r){}; // fa cose sulla stanza
