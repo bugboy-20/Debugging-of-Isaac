688483a0e332103b4132f2599bc847a0081d98f4
jantizio <emanuele0246@gmail.com>
2022-08-18 12:59:23 +0200

    il muro era stato stampato fuori dalla mappa

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ Hostile *villan1 = new Hostile(3, n2, 3, 5, {60, 7}, '=', desc2);
 Hostile *villan2 = new Hostile(3, n3, 7, 5, {20, 10}, '$', desc3);
 Hostile *villan3 = new Hostile(3, n4, 10, 5, {22, 4}, ')', desc4);
 Core *rock = new Core({35, 7}, 'O', desc5);
-Wall *w1 = new Wall({{ROOM_HEIGHT / 2, ROOM_WIDTH / 4}, false, ROOM_WIDTH / 4}, '#', desc6);
+Wall *w1 = new Wall({{ROOM_WIDTH / 2, ROOM_HEIGHT / 4}, false, ROOM_WIDTH / 4}, '#', desc6);
 
 
 Player *player = new Player(10, 10, NULL, NULL, n, 6, 5, {20, 15}, '@', desc);
