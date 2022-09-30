8cfb894128188c3628644a8a2138454ea20ec019
SaadMedhat <saad2001f@gmail.com>
2022-09-30 19:03:23 +0200

    Funzione per il cambio stanza

--- include/physics.h
+++ include/physics.h
@@ -4,9 +4,11 @@
 
 bool collision(int x, int y, Room& r);
 
-bool door_collision(int x, int y, Room& r);
+bool door_collision(coords pos, Room& r);
 
 bool general_collision(coords pos, Room& r);
+
+bool room_change(coords pos, Room& r);
 
 void do_room(Room *r); // fa cose sulla stanza
 
--- src/physics.cpp
+++ src/physics.cpp
@@ -1,37 +1,52 @@
 #include <cstddef>
+#include <string.h>
 #include "physics.h"
 #include "Events.hpp"
-
+#include "Hostile.hpp"
+#include <ncursesw/ncurses.h>
 bool collision(int x, int y, Room& r)
 {
-    bool flag;
+    bool flag1, flag2, flag3;
     coords pos;
 
     pos.x = x;
     pos.y = y;
-
-    
-    flag = door_collision(x, y, r);
-    if(flag) return flag;
-    flag = general_collision(pos, r);
-    
-    return flag;
+    flag1 = door_collision(pos, r);
+    flag2 = general_collision(pos, r);
+    flag3 = room_change(pos, r);
+    if(flag1 || flag2) return true;
+    else return false;
 }
 
-bool door_collision(int x, int y, Room& r){
-    if(y < 1) return true;
-    else if(x < 1) return true;
-    else if(y > ROOM_HEIGHT - 2) return true;
-    else if( x > ROOM_WIDTH - 2) return true;
+bool door_collision(coords pos, Room& r){
+    if(pos.y < 1) return true;
+    else if(pos.x < 1) return true;
+    else if(pos.y > ROOM_HEIGHT - 2) return true;
+    else if( pos.x > ROOM_WIDTH - 2) return true;
     else return false;
 }
 
 bool general_collision(coords pos, Room& r){
+
     if(r.get_element_in_this_position(pos) == NULL){
         return false;
-    }else return true;
+    }else{
+        return true;
+    }
+    
 }
 
+bool room_change(coords pos, Room& r){
+    if(pos.x == 0 && pos.y == (ROOM_HEIGHT/2) || pos.x == 0 && pos.y == (ROOM_HEIGHT/2) - 1){  
+        r.add_event(new RoomChangedE());
+    }else if(pos.x == ROOM_WIDTH-1 && pos.y == (ROOM_HEIGHT/2) || pos.x ==  ROOM_WIDTH-1 && pos.y == (ROOM_HEIGHT/2) - 1){
+        r.add_event(new RoomChangedE());
+    }else if(pos.x == ROOM_WIDTH/2 && pos.y == 0 || pos.x ==  (ROOM_WIDTH/2) - 1 && pos.y == 0){
+        r.add_event(new RoomChangedE());
+    }else if(pos.x == ROOM_WIDTH/2 && pos.y == (ROOM_HEIGHT-1) || pos.x ==  (ROOM_WIDTH/2) - 1 && pos.y == (ROOM_HEIGHT-1)){
+        r.add_event(new RoomChangedE());
+    }
+}
 void do_room(Room *r){}; // fa cose sulla stanza
 
 bool game_over(Player p)
