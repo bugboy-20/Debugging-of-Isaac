27722d601b97752a073db40c56480112694b281a
jantizio <emanuele0246@gmail.com>
2022-10-15 17:12:25 +0200

    EntityMoveE costruttore giusto in bullet

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -3,19 +3,18 @@
 #include "Events.hpp"
 
 #ifdef _WIN32 // sleep fn
-#include <Windows.h>
 #include <ncursesw/ncurses.h>
 #else
-#include <unistd.h>
 #include <ncurses.h>
 #endif
+
 Bullet::Bullet(coords pos, char display, char description[]) : Core(pos,display,description,false) {}
 bool Bullet::shoot(Room *r, int x, int y){
 
     coords n_pos = {this->pos.x + x, this->pos.y + y};
     if (!collision(this->pos.x + x, this->pos.y + y, *r)){
     this->pos.y = this->pos.y + 1;
-    r->add_event(new EntityMoveE(pos, n_pos));
+    r->add_event(new EntityMoveE(pos, n_pos, this->get_display()));
     return true;
     }else return false;
     
