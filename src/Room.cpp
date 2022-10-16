a94ed939027667102ff7755d5b5987b724c225a3
bugboy-20 <diegoroberto.ammirabile@gmail.com>
2022-10-16 18:56:59 +0200

    fix cambio stanze

--- src/Room.cpp
+++ src/Room.cpp
@@ -144,7 +144,7 @@ Room *Room::next_room(enum door_pos p) {
     if (this->door[p] != NULL)
         return this->door[p]->next_room;
     else {
-        fprintf(stderr, "Errore: si è tentato di entrare per una porta inesistente");
+        fprintf(stderr, "Errore: si è tentato di entrare per una porta inesistente\n");
         exit(EXIT_FAILURE);
     }
 }
