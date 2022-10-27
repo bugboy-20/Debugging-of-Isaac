85333065866a5bb7efbdeeb852d9b4ee63723509
jantizio <emanuele0246@gmail.com>
2022-10-27 18:29:12 +0200

    aggiunti nuovi nemici

--- include/HostileList.hpp
+++ include/HostileList.hpp
@@ -1,7 +1,32 @@
 #pragma once
 #include "Hostile.hpp"
 
-class Zombie:public Hostile{
-    public:
+class Zombie : public Hostile
+{
+public:
     Zombie(coords pos);
+};
+
+class Scheletro : public Hostile
+{
+public:
+    Scheletro(coords pos);
+};
+
+class Goblin : public Hostile
+{
+public:
+    Goblin(coords pos);
+};
+
+class Fantasma : public Hostile
+{
+public:
+    Fantasma(coords pos);
+};
+
+class Slime : public Hostile
+{
+public:
+    Slime(coords pos);
 };
--- src/HostileList.cpp
+++ src/HostileList.cpp
@@ -1,6 +1,46 @@
-#include "Hostile.hpp"
 #include "HostileList.hpp"
+#include "constants.h"
 
-char zombie_name[10] = "Zombie";
-char zombie_desc[] = "test";
-Zombie::Zombie(coords pos) : Hostile(pos, 'z', zombie_desc, zombie_name, 50, 20, 6) {}
+Zombie::Zombie(coords pos) : Hostile(pos,
+                                     zombie_display,
+                                     zombie_desc,
+                                     zombie_name,
+                                     zombie_health,
+                                     zombie_damage,
+                                     zombie_tr) {}
+
+Scheletro::Scheletro(coords pos) : Hostile(
+                                       pos,
+                                       scheletro_display,
+                                       scheletro_desc,
+                                       scheletro_name,
+                                       scheletro_health,
+                                       scheletro_damage,
+                                       scheletro_tr) {}
+
+Goblin::Goblin(coords pos) : Hostile(
+                                 pos,
+                                 goblin_display,
+                                 goblin_desc,
+                                 goblin_name,
+                                 goblin_health,
+                                 goblin_damage,
+                                 goblin_tr) {}
+
+Fantasma::Fantasma(coords pos) : Hostile(
+                                     pos,
+                                     fantasma_display,
+                                     fantasma_desc,
+                                     fantasma_name,
+                                     fantasma_health,
+                                     fantasma_damage,
+                                     fantasma_tr) {}
+
+Slime::Slime(coords pos) : Hostile(
+                               pos,
+                               slime_display,
+                               slime_desc,
+                               slime_name,
+                               slime_health,
+                               slime_damage,
+                               slime_tr) {}
