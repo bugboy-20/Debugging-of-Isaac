0a6d378c782d93aa5dda8b9f66a72fdb74c72fd2
Diego R. Ammirabile <diegoroberto.ammirabile@gmail.com>
2022-11-04 13:51:07 +0100

    Aggiunto costruttore con livello utile per fare nemici che scalano di difficoltà

--- src/Hostile.cpp
+++ src/Hostile.cpp
@@ -1,13 +1,19 @@
 #include "Hostile.hpp"
 
+Hostile::Hostile(coords pos, char display, char description[], char name[10], int health, int damage, int trigger_radius, int level, int health_scaling, int damage_scaling) : Entity(pos, display, description, name, health, damage)
+{
+    this->health+=level*health_scaling;
+    this->damage+=damage*damage_scaling;
+    this->trigger_radius = trigger_radius;
+}
+ 
 Hostile::Hostile(
     coords pos,
     char display,
     char description[],
-    char name[10], int health,
-    int damage, int trigger_radius) : Entity(pos, display, description, name, health, damage)
-{
-    this->trigger_radius = trigger_radius;
-}
+    char name[10],
+    int health,
+    int damage,
+    int trigger_radius) : Hostile (pos,display,description,name,health,damage,trigger_radius,0,0,0) {}
 
 int Hostile::get_trigger_radius() { return this->trigger_radius; }
