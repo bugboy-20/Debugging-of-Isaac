7a73f4bd754a61d7c6cfcc805ff17e0304480a09
jantizio <emanuele0246@gmail.com>
2022-11-22 18:13:24 +0100

    fix dovuto ad un errore nel merge

--- src/HostileList.cpp
+++ src/HostileList.cpp
@@ -1,13 +1,12 @@
 #include "HostileList.hpp"
 #include "constants.h"
 
-
 // concatena i due elementi in input mettendo il simbolo _ in mezzo
 #define mc(n, c) n##_##c
 
 #define mob(name)                              \
     char name##_string[STR_LENGTH] = name##_s; \
-    name::name(coords pos) : Hostile(pos, mc(name, display), mc(name, string), mc(name, string), {mc(name, damage), mc(name, health), mc(name, as), mc(name, ms), mc(name, range)}, mc(name, tr)) {}
+    name::name(coords pos, int level) : Hostile(pos, mc(name, display), mc(name, string), mc(name, string), {mc(name, damage), mc(name, health), mc(name, as), mc(name, ms), mc(name, range)}, mc(name, tr)) {}
 
 mob(Zombie);
 mob(Scheletro);
@@ -15,14 +14,12 @@ mob(Goblin);
 mob(Fantasma);
 mob(Slime);
 
-
-
 // Hostile che implementano i livelli
 
 #undef mob
-#define mob(name) \
-    char name ## _string[STR_LENGTH] = name ## _s; \
-    name :: name (coords pos, int level) : Hostile(pos, mc(name,display), mc(name,string), mc(name,string), {mc(name, damage), mc(name, health), mc(name, as), mc(name, ms), mc(name, range)}, mc(name,tr), level, mc(name,hs), mc(name,ds)) {}
+#define mob(name)                              \
+    char name##_string[STR_LENGTH] = name##_s; \
+    name::name(coords pos, int level) : Hostile(pos, mc(name, display), mc(name, string), mc(name, string), {mc(name, damage), mc(name, health), mc(name, as), mc(name, ms), mc(name, range)}, mc(name, tr), level, mc(name, hs), mc(name, ds)) {}
 
 /*
 
