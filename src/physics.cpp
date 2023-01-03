4bd7c42db30f1b4bed664680a37d84e0199e57f8
SaadMedhat <saad2001f@gmail.com>
2023-01-03 11:51:47 +0100

    Funzione per gestire il drop dei nemici

--- src/physics.cpp
+++ src/physics.cpp
@@ -111,6 +111,11 @@ void player_damage(Room& r, Bullet *b){
 void entity_damage(Room& r, Bullet *b, Entity *e){
     e->set_health(e->get_health() - r.p->get_damage());   
     if(e->get_health() <= 0){
+        if(((Hostile*) e)->get_drop() != NULL){
+            ItemOnGround *i = new ItemOnGround({e->get_x(), e->get_y()}, ((Hostile*) e)->get_drop());
+            r.add_items_on_ground(i);
+            r.add_event(new ItemDroppedE({e->get_x(), e->get_y()}));
+        }
         r.p->set_score(e->get_damage() + r.p->get_max_health());
         r.add_event(new ScoreChangedE(r.p));
         r.delete_room_menber(e);
